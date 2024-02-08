f643b87c14264af39ae5e2a1b13e224f4719605e
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-02-08 16:11:30 -0500

    Comments were added

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ require('./database');
 // start function
 async function main(){
     await app.listen(app.get('port'));
+
+    // Message for when server is running
     console.log(`Server on port ${app.get('port')}`);
 }
 
