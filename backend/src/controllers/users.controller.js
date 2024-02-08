51107c18aeb496db20659e9bd574835a96daaa04
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-02-08 18:13:38 -0500

    Users controller was implemented

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,11 +1,43 @@
 
 const usersCtrl = {};
 
-usersCtrl.getUsers = (req, res)=> res.json({message : 'GET Request'});
-usersCtrl.createUser = ((req, res)=> res.json({message : 'POST Request'}));
-usersCtrl.getUser = (req, res) => res.json({title : 'nota'});
-usersCtrl.updateUser = (req, res) => res.json({message : 'userUpdated'});
-usersCtrl.deleteUser = (req, res) => res.json({id : '2342563'});
+const User = require('../models/User');
+
+usersCtrl.getUsers = async (req, res)=> {
+
+    const users = await User.find();
+    res.json(users);
+
+}
+
+usersCtrl.createUser = async (req, res)=> {
+
+    const newUSer = new User(req.body);
+    await newUser.save();
+    res.json({message : 'The User was create'});
+
+}
+
+usersCtrl.getUser = async (req, res) => {
+
+    const user = await User.findById(req.params.id);
+    res.json(user);
+
+}
+
+usersCtrl.updateUser = async (req, res) => {
+
+    const updatedUser = User.findByIdAndUpdate(req.params.id, req.body);
+    res.json(updatedUser);
+
+}
+
+usersCtrl.deleteUser = async (req, res) => {
+
+    await Note.findByIdAndDelete(req.params.id);
+    res.json({message : 'The user was deleted'});
+
+}
 
 
 module.exports = usersCtrl;
